584a949738efde34c766423a359d69b253a43415
Webe1104 <webe1104@hotmail.com>
2015-03-29 13:37:45 -0600

    havad

--- app/models/mechanism_type.rb
+++ app/models/mechanism_type.rb
@@ -3,4 +3,14 @@ class MechanismType < ActiveRecord::Base
 	# Expedientes
 	has_and_belongs_to_many :patient_records
 
+	# Opciones para el combobox
+	def self.options_for_select
+		options = Array.new
+		MechanismType.all.each do | mechanism_type |
+			option = [ mechanism_type.name, mechanism_type.id.to_s ]
+			options.push(option)
+		end
+		return options
+	end
+
 end

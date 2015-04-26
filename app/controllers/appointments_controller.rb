ad1b789961ded8c9c59ee05dd9a3053ff4ee7b4f
diegoMontesinos <chesp.gommont@gmail.com>
2015-04-26 13:21:52 -0500

    Avance citas. Diego

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -21,6 +21,38 @@ class AppointmentsController < ApplicationController
 
 	# PUT
 	def update
+
+		# debug
+		ap params
+
+		# Obtenemos la cita
+		@appointment = Appointment.find(params[:id])
+
+		# Limpiamos los sintomas vacios
+		params[:appointment][:symptoms_attributes].reject! do | key, value |
+			value[:symptom_type_id] == "" and value[:level] == ""
+		end
+
+		# Hacemos update
+		@appointment.update(appointment_params)
+		if @appointment.valid?
+
+			# Mandamos a renderear de nuevo con mensaje
+			flash[:notice] = "¡Información de cita guardada!"
+
+			# Redirigimos al expediente
+			redirect_to havad_index_path
+		else
+
+			# Redirigimos al show
+			redirect_to show_appointment_path(params[:id])
+		end
 	end
+
+	private
+
+		def appointment_params
+			params.require(:appointment).permit(:attended, :notes, :symptoms_attributes => [:symptom_type_id, :level])
+		end
 
 end
--- app/models/symptom_type.rb
+++ app/models/symptom_type.rb
@@ -3,4 +3,21 @@ class SymptomType < ActiveRecord::Base
 	# Sintoma de una cita
 	has_many :symptoms
 
+	# Opciones para el combobox
+	def self.options_for_select
+		options = Array.new
+		
+		# Agregamos la primera opcion
+		options.push(["-", nil])
+
+		# Agregamos las demas opciones
+		SymptomType.all.each do | symptom_type |
+			option = [ symptom_type.name, symptom_type.id.to_s ]
+			options.push(option)
+		end
+
+		# Regresamos las opciones
+		return options
+	end
+
 end

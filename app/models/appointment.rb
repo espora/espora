d57d6ca4083f4139d5849ffe5e379781e76dcda6
diegoMontesinos <chesp.gommont@gmail.com>
2015-05-04 13:19:21 -0500

    Ultimos cambios. Ups

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,12 +1,13 @@
 class Appointment < ActiveRecord::Base
 
-	# Pertenece a un expediente
+	# Expediente
 	belongs_to :patient_record
 
 	# Sintomas detectados
 	has_many :symptoms, :dependent => :destroy
+
+	# Sintomas en formularios anidados
 	accepts_nested_attributes_for :symptoms, :allow_destroy => true,
 	:reject_if => proc { |attrs| attrs["symptom_type_id"].blank? or attrs["level"].blank? }
-
 
 end

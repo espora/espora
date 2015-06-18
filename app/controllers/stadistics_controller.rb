6ccbaa6fb47851a060a74eb0bdfcc1aa6601539b
LuzElena <luzelenajb@ciencias.unam.mx>
2015-06-18 10:14:58 -0500

    Estadisticas graficas.Luz

--- app/controllers/stadistics_controller.rb
+++ app/controllers/stadistics_controller.rb
@@ -15,174 +15,215 @@ class StadisticsController < ApplicationController
 	def graph
 
 		case params[:graph]
-		when 'SolicitudesVsAtendidos '
-			@graphAtendidos = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Solicitudes y de alumnos atendidos",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
+		when 'SolicitudesVsAtendidos'
+			@graph = Gchart.line(
+                    :size   => '600x400',
+                    :title  => "Número de solicitudes vs alumnos atendidos",
+                    :legend => ['Número de alumnos atendidos', 'Número de solucitudes recibidas'],
+                    :bar_colors => ['000000', '0088FF'],
+                    :data   => [[47, 55, 87, 85, 95],[47, 80, 129, 176, 202]],
+                    :bg => 'EFEFEF',
+                   	:legend_position => 'bottom',
+                   	:axis_with_labels => [['x'], ['y']],
+                   	:max_value => 202,
+                   	:min_value => 0,
+                   	:axis_labels => [["Semestre 2011-2|Semestre 2012-1|Semestre 2012-2|Semestre 2013-1|Semestre 2013-2"]]
+            )
+
+		when 'SexoSol'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Sexo de Solicitantes",
+				:legend => ['Hombres', 'Mujeres'],
+				:custom => "chco=8856a7,9ebcda",
+				:data   => [120, 245],
+				:labels => ["120", "245"],
+				:bg => 'EFEFEF'
 			)
-		end
-		case params[:graph]
-		when 'Sexo'
-			@graphSexo = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
+		when 'SexoAten'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Sexo de Atendidos",
+				:legend => ['Hombres', 'Mujeres'],
+				:custom => "chco=8856a7,9ebcda",
+				:data   => [34, 80],
+				:labels => ["34", "80"],
+				:bg => 'EFEFEF'
 			)
-		end
-		case params[:graph]
-		when 'Edad'
-			@graphEdad = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
-			)
-		end
-		case params[:graph]
+
 		when 'Familiar'
-			@graphFamiliar = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Estructura Familiar",
+				:legend => ['Con ambos padres', 'Solo con madre','Solo padre', 'Otros familiares', 'Amigos', 'Pareja'],
+				:custom => "chco=fa9fb5fa9fb5, c51b8ac51b8a",
+				:data   => [53, 33, 3, 5, 5, 1],
+				:labels => ["53%", "33%", "3%", "5%", "5%", "1%"],
+				:bg => 'EFEFEF'
 			)
-		end
-		case params[:graph]
-		when 'Procedencia'
-			@graphProcedencia = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
+
+		when 'CarreraSol'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Carrera de precedencia de alumnos solicitantes",
+				:legend => ['Bilogia', 'Actuaría', 'Física', 'Matemáticas', 'Ciencias de la Computación', 'Ciencias de la Tierra'],
+				:custom => "chco=43a2ca,e0f3db",
+				:data   => [53, 33, 3, 5, 5, 1],
+				:labels => ["53%", "33%", "3%", "5%", "5%", "1%"],
+				:bg => 'EFEFEF'
 			)
-		end
-		case params[:graph]
-		when 'Semestre'
-			@graphSemestre = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
+
+		when 'CarreraAten'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Carrera de precedencia de alumnos atendidos",
+				:legend => ['Bilogia', 'Actuaría', 'Física', 'Matemáticas', 'Ciencias de la Computación', 'Ciencias de la Tierra'],
+				:custom => "chco=43a2ca,e0f3db",
+				:data   => [39, 21, 18, 12, 4, 7],
+				:labels => ["39%", "21%", "18%", "12%", "4%", "7%"],
+				:bg => 'EFEFEF'
 			)
-		end
-		case params[:graph]
-		when 'Materias'
-			@graphMaterias = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
+
+		when 'SemestreSol'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Semestre de precedencia de alumnos solicitantes",
+				:legend => ['1°', '2°', '3°', '4°', '5°', '6°', '7°', '8°'],
 				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
+				:data   => [39, 21, 18, 12, 4, 3, 2, 1],
+				:labels => ["39%", "21%", "18%", "12%", "4%", "3%", "2%", "1%"],
+				:bg => 'EFEFEF'
 			)
-		end
-		case params[:graph]
+
+		when 'SemestreAten'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Semestre de precedencia de alumnos atendidos",
+				:legend => ['1°', '2°', '3°', '4°', '5°', '6°', '7°', '8°'],
+				:custom => "chco=FFF110,FF0000",
+				:data   => [39, 21, 18, 12, 4, 3, 2, 1],
+				:labels => ["39%", "21%", "18%", "12%", "4%", "3%", "2%", "1%"],
+				:bg => 'EFEFEF'
+			)
+
+		when 'MateriasSol'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Adeudo de materias de solicitantes",
+				:legend => ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+				:custom => "chco=FFF110,FF0000",
+				:data   => [39, 21, 18, 12, 4, 3, 2, 1],
+				:labels => ["39%", "21%", "18%", "12%", "4%", "3%", "2%", "1%"],
+				:bg => 'EFEFEF'
+			)
+
+		when 'MateriasAten'
+			@graph = Gchart.pie(
+				:size   => '600x400',
+				:title  => "Adeudo de materias de atendidos",
+				:legend => ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+				:custom => "chco=FFF110,FF0000",
+				:data   => [39, 21, 18, 12, 4, 3, 2, 1],
+				:labels => ["39%", "21%", "18%", "12%", "4%", "3%", "2%", "1%"],
+				:bg => 'EFEFEF'
+			)
+
+
+
+
+
+
+
+
 		when 'Eficiencia'
-			@graphEficiencia = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.bar(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'Motivos'
-			@graphMotivos = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'Sintomas'
-			@graphSintomas = Gchart.pie(
-				:size   => '300x200',
-				:title  => "Grafica de Pastel- Carreras de la facultad",
-				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
-				:custom => "chco=FFF110,FF0000",
-				:data   => [120, 45, 25, 55, 20]
-			)
-		end
-		case params[:graph]
+			@graph = Gchart.bar(
+            :size => '800x00',
+            :bar_colors => ['000000'],
+            :title => "Signos y síntomas",
+            :bg => 'EFEFEF',
+            :legend => ['Tristeza ', 'Autoestima', 'Insatisfacción', 'Enojo', 'Estrés', 'Dif. estudiar', 'Ansiedad', 'Prob. para relacionarse', 'Exigencias elevadas', 'Desesperanza', 'Miedo a participar', 'Ideas suicidas', 'Intento de suicidio'],
+            :data => [83, 66, 65.7, 33.8, 60.8, 60.1, 55.5, 49.4, 29, 28.6, 20, 15.1, 3],
+            :encoding => 'simple',
+            :stacked => false,
+            :legend_position => 'bottom',
+            :axis_with_labels => [['x'], ['y']],            
+            :max_value => 84,
+            :min_value => 0,
+            :axis_labels => [["83%|66%|65.7%|33.8%|60.8%|60.1%|55.5%|49.4%|29%|28.6%|20%|15.1%|3%]"]]
+            )   
 		when 'AreasAfectadas'
-			@graphAfectadas = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'AreasBeneficiadas'
-			@graphBeneficiadas = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'EdoEmocionalAntes'
-			@graphEmoAntes = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'EdoEmocionalDespues'
-			@graphEmoDespues= Gchart.pie(
-				:size   => '300x200',
+			@graph= Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'Ayudo'
-			@graphAyudo = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'Difusion'
-			@graphDifusion = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
-		end
-		case params[:graph]
 		when 'Recomendacion'
-			@graphRecomendacion = Gchart.pie(
-				:size   => '300x200',
+			@graph = Gchart.pie(
+				:size   => '600x400',
 				:title  => "Grafica de Pastel- Carreras de la facultad",
 				:legend => ['Biologia', 'Matematicas', 'Ciencias de la computacion', 'Actuaria', 'Fisica'],
 				:custom => "chco=FFF110,FF0000",
 				:data   => [120, 45, 25, 55, 20]
 			)
+		else
 		end
 
 		render partial: "graph"

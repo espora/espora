5d1c3f8d176a243c85a0e893a6d95043badceb9e
Diego Montesinos <diegoa.montesinos@gmail.com>
2016-03-28 12:47:45 -0600

    Fix remove string from a migrationo

--- db/migrate/20141217180457_remove_telephone_from_therapist.rb
+++ db/migrate/20141217180457_remove_telephone_from_therapist.rb
@@ -1,6 +1,5 @@
 class RemoveTelephoneFromTherapist < ActiveRecord::Migration
   def change
     remove_column :therapists, :telephone, :string
-    remove_column :therapists, :string, :string
   end
 end
